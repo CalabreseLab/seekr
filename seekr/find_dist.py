e97dc47dc64bcd867c41286a5824f47c93108cf6
Warmful <ni.luke2011@gmail.com>
2023-09-12 19:38:13 -0400

    Added functions to compare lncRNA using p-values

---
+++ seekr/find_dist.py
@@ -0,0 +1,269 @@
+####################################################################################################################
+### Description: 
+# Find the best fitted distribution to the input background sequences 
+
+### Details:
+# data to be fitted -- all possible pairwise kmer pearson correlation for the background sequences
+# can also choose to return the actual data by itself
+# the results (best fitted distribution or actual data) will be used to calculate p-values
+
+### Input:
+# inputseq: fasta file containing background sequences. By default (inputseq='default') use mouse vM25 Long non-coding RNA transcript sequences
+# https://www.gencodegenes.org/mouse/release_M25.html
+# k_mer: k-mer size, by default k_mer=4
+# log2: whether to do log2 transform, possible options are: 'Log2.post','Log2.pre' and 'Log2.none', default is 'Log2.post'
+# Log2.post -- Log2 transformation post-standardization: self.counts += np.abs(np.min(self.counts)) self.counts += 1 self.counts = np.log2(self.counts)
+# Log2.pre -- Log2 transformation pre-standardization: self.counts += np.abs(np.min(self.counts)) self.counts += 1
+# Log2.none -- no log2 transformation
+# models: groups of candidate models for fitting, options are: 'all','common10' (default), or a list of distributions by user input for example ['norm','expon','pareto']
+# 'all' - fit all available distributions from scipy stats, both rv_continuous and rv_discrete: https://docs.scipy.org/doc/scipy/reference/stats.html#continuous-distributions
+# 'common10' - fit 10 common distributions ('cauchy', 'chi2', 'expon', 'exponpow', 'gamma', 'lognorm', 'norm', 'pareto', 'rayleigh', 'uniform')
+# common10 distributions are deteremined from get_common_distributions() from the Fitter library with 'powerlaw' replaced by 'pareto' as pareto belongs to powerlaw family and bears a biology relation 
+# user can also choose to input their own list of distributions in a list format, but be sure all distributions are available in scipy.stats
+# subsetting: True (default) or False. whether or not to use a subset of the data for fitting or output. For large datasets, subsetting is recommended to save time
+# subset_size: the size of the subset to use for fitting or output. Default is 100000. only be used when subsetting=True
+# id subset_size is larger than the actual data size, the actual data size will be used instead
+# fit_model: True (default) or False. whether or not to fit the data to the distributions sepcified in models. 
+# For small dataset, you can set fit_model=False, this will return the actual data as a npy array, without fitting to any distributions
+# in this way, the p values will be calculated based on the actual data instead of the fitted distribution
+# statsmethod: the stats method used to quantify the goodness of fit of the distributions vs data
+# options are: 'ks' (default, Kolmogorov-Smirnov test), 'mse' (Mean Squared Error), 'aic' (Akaike Information Criterion), 'bic' (Bayesian Information Criterion)
+# smaller values returned by all four statsmends generally indicate a better fit
+# KS: measures the largest difference between the observed cumulative distribution function (CDF) of the data and the expected CDF of the model
+# it is sensitive to differences in both the center and tails of the distribution and it provides an absolute measure of goodness-of-fit, which is intuitive and easy to understand
+# it can be sensitive to the sample size, and it may not have sufficient power to detect subtle differences in distributions when the sample size is small
+# it does not penalize for model complexity, so overfitting could be a potential problem
+# MSE: is an error-based metric, calculates the mean squared error between the actual data and the fitted data, it is intuitive and easy to understand
+# but it is sensitive to outliers, and it does not take into account the number of parameters in the model
+# Both AIC and BIC are likelihood based metrics which has some advantages over error-based metrics:
+# they balance the goodness of fit against the complexity of the model; they have broader applicability especially for models where the mean and variance are not well-defined (such as cauchy)
+# AIC: Tends to favor models that fit the data very closely. It can sometimes choose overly complex models
+# BIC: Incorporates a penalty term for the number of parameters in the model and the sample size, and therefore can result in the selection of simpler models compared to AIC
+# but AIC and BIC takes longer time to calculate and the output of AIC and BIC are unitless, they are good for comparing different models rather than providing an interpretable absolute measure of "goodness of fit"
+# The KS test measures how well the model fits the entire data distribution. MSE is designed to measure the predictive accuracy of a model but doesn't account for model complexity. AIC and BIC are designed for model selection, balancing fit and complexity.
+# MSE is sensitive to outliers, while AIC and BIC can also be influenced by them depending on the likelihood formulation. The KS test is less sensitive to outliers but more focused on the overall shape of the distribution
+# If not sure which statsmethod works for the input data, try several of them to get a well-rounded view and set plotfit=True to see the fitted distributions vs the actual data
+# progress_bar: True or False (default). whether or not to show the progress bar when fitting the data to the distributions.
+# plotfit: True (default) or False. whether or not to plot the fitted distributions (red dash line) vs the actual data (blue solid line) for all distributions in models.
+# if set to True, the plot will be saved as a pdf file named 'fitted_model_plots.pdf' in the current directory
+
+### Output:
+# If fit_model is True (fit ditributions to the data), returns the distributions name, goodness of fit (like D stats in ks) and fitted params for all distributions in models
+# the results here is ordered by the goodness of fit, the first distribution (results[0]) is the best fitted distribution (to be sure, use manual inspection by setting plotfit=True)
+# If fit_model is False (do not fit distributions to the data), returns the actual data (with or without subsetting based on subsetting argument) as a npy array, without fitting to any distributions
+
+### Example:
+# fitres = find_dist(inputseq='default', k_mer=4, models='common10', subsetting=True, subset_size = 10000, fit_model=False, statsmethod='ks',progress_bar=True, plotfit=True)
+
+####################################################################################################################
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import stats
+from scipy.stats import kstest
+import warnings
+
+from seekr.kmer_counts import BasicCounter as seekrBasicCounter
+from seekr.pearson import pearson as seekrPearson
+
+from tqdm import tqdm
+
+
+
+# Finds and returns distributions and respective parameters that best fit input sequences
+def find_dist(inputseq='default', k_mer=4, log2='Log2.post', models='common10', subsetting=True, subset_size = 100000, fit_model=True, statsmethod='ks',progress_bar=False, plotfit=True):
+
+    #Determine what background transcriptome to use
+    if inputseq == 'default':
+        print('Using default background sequences: mouse vM25 Long non-coding RNA transcript sequences from Gencode.')
+        print('https://www.gencodegenes.org/mouse/release_M25.html')
+        print('Only including unique sequences and have the full length Airn(chr17:12741398-12830151) appended.')
+        inputseq = 'gencode.vM25.lncRNA_transcripts.unique.genesequence_withfullairn.fa'
+    
+    #preprare the list of distributions to be used
+    if models == 'common10':
+        distributions = ['cauchy', 'chi2', 'expon', 'exponpow', 'gamma', 
+                         'lognorm', 'norm', 'pareto', 'rayleigh', 'uniform']
+        
+    else: 
+        #Create all distributions (continous and discrete) that would be used
+        continuous_distributions = [d for d in dir(stats) if
+                        isinstance(getattr(stats, d), stats.rv_continuous)]
+
+        discrete_distributions = [d for d in dir(stats) if
+                                isinstance(getattr(stats, d), stats.rv_discrete)]
+
+        distributions = continuous_distributions + discrete_distributions 
+
+        # it is a convention to start the name of private or special methods with an underscore
+        # this line is used to filter out private or special methods from the scipy.stats module
+        distributions = [d for d in distributions if d[0] != '_']
+        # remove levy_stable and studentized_range, it has problem converging
+        # and the shape of the distribution is not what we want
+        distributions = [d for d in distributions if d != 'levy_stable']
+        distributions = [d for d in distributions if d != 'studentized_range']
+        # if 'gilbrat' is in distributions, replace 'gilbrat' with 'gibrat' as 'gilbrat' is a misspelling of 'gibrat'
+        if 'gilbrat' in distributions:
+            distributions[distributions.index('gilbrat')] = 'gibrat'
+
+        if models != 'all':
+            # check if the user input distributions are included in the distributions list
+            orilen1=len(models)
+            distributions = [d for d in models if d in distributions]
+            if len(distributions) < orilen1:
+                print("Please enter valid distribution names available in scipy.stats. refer to https://docs.scipy.org/doc/scipy/reference/stats.html#continuous-distributions")
+                # find out which distributions are excluded
+                excluded1 = [d for d in models if d not in distributions]
+                print(f"Excluding invalid distributions for fitting: {excluded1}")
+        
+
+    # Convert distribution names to distribution objects
+    tempdist = distributions.copy()
+    orilen2=len(distributions)
+    distributions = [getattr(stats, d) for d in distributions]
+
+    # Filter distributions - some distributions can throw errors during the fit step
+    # filter out the distributions that do not have a 'fit' method
+    distributions = [d for d in distributions if 'fit' in dir(d)]
+    if (len(distributions) < orilen2) and (models != 'all' and models != 'common10') :
+        # find out which distributions are excluded
+        excluded2 = [d for d in tempdist if d not in distributions]
+        print(f"Excluding distributions do not have a 'fit' method: {excluded2}")
+
+
+    #Create normalization mean and std vector for the inputseq
+    bkg_norm_counter = seekrBasicCounter(inputseq,log2=log2,k=k_mer,silent=True)
+    bkg_norm_counter.get_counts()
+    mean_path = f'bkg_mean_{k_mer}mers.npy'
+    std_path = f'bkg_std_{k_mer}mers.npy'
+    np.save(mean_path, bkg_norm_counter.mean)
+    np.save(std_path, bkg_norm_counter.std)
+
+    # Count k-mers
+    bkg_counter = seekrBasicCounter(inputseq,mean=mean_path,std=std_path,k=k_mer,silent=True)
+    bkg_counter.make_count_file()
+
+    # Find similarities
+    sim_counts = seekrPearson(bkg_counter.counts,bkg_counter.counts)
+
+    # Extract the upper triangle and flatten it
+    sim_triu = sim_counts[np.triu_indices(sim_counts.shape[0], k=1)]
+
+    #If the user wants to subset the data
+    if subsetting: 
+        if len(sim_triu)>subset_size:
+            # randomly sample the user defined number of the sim_triu
+            sim_triu = np.random.choice(sim_triu, size=subset_size, replace=False)
+        else:
+            print("subset_size is larger than the actual data size, use the actual data size instead")
+    
+    
+
+    #if fitting chosen, fit the data to the distributions
+    if fit_model:
+        
+        if len(distributions)>50 and len(sim_triu)>5000000 and subsetting==False:
+            print("The input sequence count and distribution number for fitting are both large, subsetting is recommended to save time")
+    
+        results = []
+        # set iterable based on progress_bar
+        iterable = tqdm(distributions) if progress_bar else distributions
+        # Fit distributions to data
+        for distribution in iterable:
+            # Try to fit the distribution
+            # Ignore warnings from data that can't be fit
+            with warnings.catch_warnings():
+                warnings.filterwarnings('ignore')
+
+                # fit dist to data
+                try: 
+                    params = distribution.fit(sim_triu)
+
+                    if statsmethod == 'ks':
+                        # Get the KS test result on fitted data
+                        D, p = kstest(sim_triu, distribution.name, args=params)
+
+                    elif statsmethod == 'mse':
+        
+                        if isinstance(distribution, stats.rv_continuous):
+                            synthetic_data = distribution.rvs(*params, size=len(sim_triu))
+                        else:
+                            synthetic_data = distribution.rvs(*params[:-2], loc=params[-2], scale=params[-1], size=len(sim_triu))
+                            
+                        residuals = sim_triu - synthetic_data
+                        D = np.mean(residuals**2)
+
+
+                    elif statsmethod == 'aic' or statsmethod == 'bic':
+                        # Calculate log-likelihood
+                        if isinstance(distribution, stats.rv_continuous):
+                            log_likelihood = np.sum(distribution.logpdf(sim_triu, *params))
+                        else:
+                            log_likelihood = np.sum(distribution.logpmf(sim_triu, *params[:-2], loc=params[-2], scale=params[-1]))
+                        
+                        # Number of parameters
+                        k = len(params)
+                        # Number of data points
+                        n = len(sim_triu)
+                        
+                        # Calculate AIC and BIC
+                        if statsmethod == 'aic':
+                            D = 2 * k - 2 * log_likelihood
+
+                        else: 
+                            D = np.log(n) * k - 2 * log_likelihood
+
+                    else:
+                        print("Please enter a valid statsmethod: 'ks', 'mse', 'aic', or 'bic'. Use default 'ks' now.")
+                        D, p = kstest(sim_triu, distribution.name, args=params)
+
+                # catch the error if the distribution cannot be fit, exclude it from the results
+                except Exception as e:
+                    print(f"Could not fit {distribution.name} because {e}, excluding it from the results")
+                    continue
+
+                # save the name and test result
+                results.append((distribution.name, D, params))
+
+        # Sort by minimum D statistics
+        results.sort(key=lambda x: x[1])
+        # sort by p val
+        # results.sort(key=lambda x: x[2], reverse=True)
+
+        if plotfit:
+            n = len(results)
+            n_cols = 5  # 5 columns
+            n_rows = n // n_cols + (n % n_cols > 0)   # Calculate the number of rows needed
+
+            fig, axes = plt.subplots(n_rows, n_cols, figsize=(15, n_rows*3))  # Increase figure size as necessary
+            axes = axes.ravel()  # Flatten the axes array
+
+            # Generate PDF for each distribution and plot it
+            for idx, (ax, result) in enumerate(zip(axes, results)):
+                dist_name, Dval,params = result
+                # print(dist_name)
+                distribution = getattr(stats, dist_name)
+                
+                # Generate PDF
+                x = np.linspace(min(sim_triu), max(sim_triu), 1000)
+                pdf = distribution.pdf(x, *params)
+
+                # Plot the original data as histogram and the fitted model
+                ax.hist(sim_triu, bins=100, density=True, alpha=0.6, color='skyblue')
+                ax.plot(x, pdf, 'r--', linewidth=2)
+                ax.set_title(f'{idx+1}: {dist_name} (Dev={Dval:.3f})')
+
+            # Remove unused subplots
+            for i in range(len(results), len(axes)):
+                fig.delaxes(axes[i])
+
+            plt.tight_layout()
+            # save plot as pdf
+            plt.savefig('fitted_model_plots.pdf',dpi=300,format='pdf')
+
+        # results is a list of tuples like (distribution_name, D_statistics, params)
+        return results
+    else:
+        if plotfit:
+            print('No plot will be produced as fit_model is set to False, please set fit_model=True to plot the fitted distributions vs the actual data')
+        return sim_triu
+
---
+++ seekr/find_pval.py
@@ -0,0 +1,176 @@
+###################################################################################################
+### Description: 
+# calculte p values of the seekr.pearson correlation values for input sequence 1 vs input sequence 2 
+# p value is based on the output of find_dist, which is either a list of distributions or a npy array
+
+### Details:
+# this function connects the output of find_dist and the input sequnces of interests (2 input sequences)
+# given the background sequencs, find_dist calculate all possible pairwise seekr.pearson values
+# and then outputs either a list of fitted distributions or the npy array of the actual data 
+# find_pval firstly calculates the seekr.pearson of the two input sequences, which produces a correlation matrix 
+# find_pval then calculate the p value for each r value in the pearson correlation matrix based on the output of find_dist
+# the output of find_pval is a dataframe of p values, with the row names (input 1) and column names (input 2) as the headers of the input sequences
+
+### Input:
+# seq1file: the path to the fasta file of input sequence 1, could be a single sequence or multiple sequences
+# seq2file: the path to the fasta file of input sequence 2, could be a single sequence or multiple sequences
+# se1file and seq2file could be the same file, in this case, the p values are calculated for the seekr correlation between each pair of sequences in the file
+# mean_path: the route to your normalization mean vector file
+# std_path: the route to your normalization std vector file
+# k_mer: the kmer size used in seekr, which should be consistent with the mean and std file
+# log2: whether to do log2 transform, possible options are: 'Log2.post','Log2.pre' and 'Log2.none', default is 'Log2.post'
+# Log2.post -- Log2 transformation post-standardization: self.counts += np.abs(np.min(self.counts)) self.counts += 1 self.counts = np.log2(self.counts)
+# Log2.pre -- Log2 transformation pre-standardization: self.counts += np.abs(np.min(self.counts)) self.counts += 1
+# Log2.none -- no log2 transformation
+# fitres: the output of find_dist, which is either a list of distributions or a npy array
+# bestfit: the index of user determined best fit distribution in the list of distributions returned by find_dist, using normal index starting from 1 (not 0), default is 1
+# save_file: whether to save the output dataframe to a csv file, default is True
+# outputname: the name of the output file, default is 'testpval.csv'
+# progress_bar: whether to show the progress bar during p value caculation, default is True
+
+### Output:
+# a dataframe of p values, with the row names (input 1) and column names (input 2) as the headers of the input sequences
+# if save_file is True, the dataframe is saved to a csv file named as outputname (default is 'testpval.csv')
+# if the fitres is not in the correct format, the output is None
+# correct format of fitres is either a npy array 
+# or a list of distributions, in the format of tuples (string, number, tuple of numbers) corresponds to (distribution name, deviance, parameters)
+
+### Example:
+# fitres = find_dist(inputseq='default', k_mer=4, models='common10', subsetting=True, subset_size = 10000, fit_model=False, statsmethod='ks',progress_bar=True, plotfit=True)
+# pvals=find_pval(seq1file='test1.fa', seq2file='test2.fa', mean_path='bkg_mean_4mers.npy', std_path='bkg_std_4mers.npy', k_mer=4, fitres=fitres, log2='Log2.post', bestfit=1, save_file=True, outputname='testpval.csv', progress_bar=True)
+
+
+########################################################################################################
+import numpy as np
+from scipy import stats
+from tqdm import tqdm
+import pandas as pd
+
+from seekr.kmer_counts import BasicCounter as seekrBasicCounter
+from seekr.pearson import pearson as seekrPearson
+from seekr.fasta_reader import Reader as seekrReader
+
+# check if fitres is a list consisting of tuples of (string, number, tuple of numbers)
+# th number here could be float64 or float32
+def is_float_type(x):
+    return isinstance(x, float) or np.isscalar(x)
+
+def check_tuple_format(tup):
+    if len(tup) != 3:
+        return False
+    return isinstance(tup[0], str) and \
+           is_float_type(tup[1]) and \
+           isinstance(tup[2], tuple) and \
+           all(is_float_type(x) for x in tup[2])
+
+def check_main_list(main_list):
+    return all(check_tuple_format(tup) for tup in main_list)
+
+
+def find_pval(seq1file, seq2file, mean_path, std_path, k_mer, fitres, log2='Log2.post', bestfit=1, save_file=True, outputname='testpval.csv', progress_bar=True):
+
+    t1 = seekrBasicCounter(seq1file, mean=mean_path, std=std_path, k=k_mer, log2=log2, silent=True)
+    t2 = seekrBasicCounter(seq2file, mean=mean_path, std=std_path, k=k_mer, log2=log2, silent=True)
+
+    t1.make_count_file()
+    t2.make_count_file()
+
+    sim = seekrPearson(t1.counts,t2.counts)
+
+    header1=seekrReader(seq1file).get_headers()
+    header2=seekrReader(seq2file).get_headers()
+    # remove the '>' in the header
+    header1 = [i[1:] for i in header1]
+    header2 = [i[1:] for i in header2]
+
+    # check if header1 only contains unique elements
+    if len(header1) != len(set(header1)):
+        print('The headers of seq1file is not unique.')
+        print('Be carefule during further analysis as there are potential indexing problems.')
+
+    # check if header2 only contains unique elements
+    if len(header2) != len(set(header2)):
+        print('The headers of seq2file is not unique.')
+        print('Be carefule during further analysis as there are potential indexing problems.')
+        
+    # check if fitres is a distribution or a npy array
+    if isinstance(fitres, list):
+        # check if fitres is a list consisting of (string, number, tuple of numbers)
+        if not check_main_list(fitres):
+            print('The format of fitres is wrong.')
+            print('fitres should be a list consisting of tuples (string, number, tuple of numbers) corresponds to (distribution name, deviance, parameters)')
+            print('fitres should be the output of find_dist.')
+            print('No p value is calculated. The output is None.')
+            return None
+        else: 
+            # get the best fit distribution by user input
+            # for python the counting starts from 0
+            bestdist = fitres[bestfit-1]
+            distname = bestdist[0]
+            params = bestdist[2]
+
+            #Load distribution
+            dist = getattr(stats, distname)
+            distribution = dist(*params)
+
+            # Initialize a matrix to hold the p-values
+            p_values = np.zeros_like(sim)
+
+            # Loop through each element in sim
+            iterable = tqdm(range(sim.shape[0]), desc="Rows") if progress_bar else range(sim.shape[0])
+            for i in iterable:
+                for j in range(sim.shape[1]):
+                    p_values[i, j] = 1 - distribution.cdf(sim[i, j])
+            # convert to dataframe and adding row/column names
+            pval_df = pd.DataFrame(p_values, index=header1, columns=header2)
+            # save the dataframe to csv file
+            if save_file:
+                pval_df.to_csv(outputname)
+
+            return pval_df
+
+
+
+    elif isinstance(fitres, np.ndarray):
+        # check if fitres is a 1D array
+        if len(fitres.shape) == 1:
+            # Calculate p-values for entire sim matrix
+            total_length_fitres = len(fitres)
+
+            # alternative method to the loop below, faster but requires more memory
+            # Use broadcasting to compare each element in sim with all elements in fitres
+            # The result is a 3D array where the third dimension has size equal to total_length_fitres
+            # comparison_result = fitres > sim[:,:,np.newaxis]
+            # Sum along the third dimension and divide by total_length_fitres to get p-values
+            # p_values = np.sum(comparison_result, axis=-1) / total_length_fitres
+
+            # Initialize a matrix to hold the p-values
+            p_values = np.zeros_like(sim)
+
+            # Loop through each element in sim
+            iterable = tqdm(range(sim.shape[0]), desc="Rows") if progress_bar else range(sim.shape[0])
+            for i in iterable:
+                for j in range(sim.shape[1]):
+                    p_values[i, j] = np.sum(fitres > sim[i, j]) / total_length_fitres
+
+            # convert to dataframe and adding row/column names
+            pval_df = pd.DataFrame(p_values, index=header1, columns=header2)
+            # save the dataframe to csv file
+            if save_file:
+                pval_df.to_csv(outputname)
+            return pval_df 
+        
+        else:
+            print('The dimension of fitres as a numpy array is wrong. fitres should be a 1D numpy array.')
+            print('fitres should be the output of find_dist.')
+            print('No p value is calculated. The output is None.')
+            return None
+
+
+    else:
+        print('fitres should be the output of find_dist. It should be either a list of distributions or a numpy array.')
+        print('No p value is calculated. The output is None.')
+        return None
+
+
+
