a39dadc987d1b4971e8a4af4393e08b27868345f
Shuang <57366173+Shuang-Plum@users.noreply.github.com>
2023-09-12 19:25:21 -0400

    barplots, leiden community and text plot upload

---
+++ seekr/kmer_leiden.py
@@ -0,0 +1,222 @@
+###########################################################################################
+### Description:
+# plot Leiden community network for input fasta seqeunces
+# it works better with small number of nodes (less than 50)
+# for better visualization and customization, please set savecsv to True and use Gephi with the saved nodes and edges files
+
+### Details:
+# take as input a fasta file with multiple sequences
+# calculte sequences distance matrix as seekr pearson correlation of the fasta file to itself
+# use Leiden community to call cluster
+# edge is weighted with the pearson correlation results
+# layout the nodes with igraph
+# the network is undirected
+
+### input:
+# inputfile: the route to your input fasta file, please ensure all input sequence has unique header, this will be used for node label
+# mean: the route to your normalization mean vector file
+# std: the route to your normalization std vector file
+# k: the kmer size used in seekr, which should be consistent with the mean and std file
+# algo: the Leiden algorithm used, default is 'RBERVertexPartition'
+# other option of algo: 'ModularityVertexPartition', 'RBConfigurationVertexPartition','RBERVertexPartition', 'CPMVertexPartition', 'SurpriseVertexPartition','SignificanceVertexPartition'
+# rs: the resolution parameter used in Leiden algorithm, default is 1.0, larger rs leads to more clusters and smaller rs leads to less clusters
+# edgecolormethod: the method used to set edge color, default is 'gradient' -- gradient scale of grey for egdes based on edge weights (pearson correlation)
+# edgecolormethod: the other option: 'threshold' -- edge color thresholded on edge weights (pearson correlation), smaller than threshold is grey, larger than threshold is black
+# edgecolormethod: if other method is passed to edgecolormethod, it will do 'gradient' as the default
+# edgethreshold: the threshold used in edgecolormethod 'threshold', default is 0.1
+# outputname: the name of your output file, default is 'test', this will broadcase to all saved images (pdf) and csv files
+# outputpath: the route to your output file, default is current working directory, example: '/Users/username/Desktop/'
+# savecsv: whether to save the nodes and edges csv files, default is True. The saved edges and nodes csv files are readily importable to Gephi for a better network visualization
+
+### output:
+# a pdf file (outputname_gradient_leiden_network.pdf or outputname_threshold_leiden_network.pdf) of the Leiden community network
+# if savecsv is True, the corresponding edges and nodes csv files (outputname_edges_leiden.csv and outputname_nodes_leiden.csv) will also be saved, which can be readily imported to Gephi
+
+### Example:
+# kmer_leiden(inputfile='testld.fa', mean='gencodevM25_unique_mean_4mers.npy',
+#             std='gencodevM25_unique_std_4mers.npy', k=4, algo='RBERVertexPartition', rs=1.0,
+#             edgecolormethod='gradient', edgethreshold=0.1, 
+#             outputpath='', outputname='test', savecsv=True)
+
+###################################################################################
+
+import pandas as pd
+import numpy as np
+import networkx as nx
+import igraph as ig
+import leidenalg
+
+
+from seekr.kmer_counts import BasicCounter as seekrBasicCounter
+from seekr.pearson import pearson as seekrPearson
+from seekr.fasta_reader import Reader as seekrReader
+
+def kmer_leiden (inputfile, mean, std, k, algo='RBERVertexPartition', rs=1.0, 
+                 edgecolormethod='gradient', edgethreshold=0.1,
+                 outputname='test', outputpath='', savecsv=True):
+
+    t1 = seekrBasicCounter(inputfile,
+                        mean=mean,
+                        std=std,
+                        k=k,silent=True)
+    t1.make_count_file()
+    header1=seekrReader(inputfile).get_headers()
+    # remove the '>' in the header
+    header1 = [i[1:] for i in header1]
+
+    # calculate pearson correlation
+    ld_sim = seekrPearson(t1.counts,t1.counts)
+
+    # set all negative values to 0 in test_sim
+    ld_sim[ld_sim<0] = 0
+    # set diagonal values to 0
+    np.fill_diagonal(ld_sim, 0)
+
+    # convert to pandas dataframe with row and column names as headers
+    df = pd.DataFrame(ld_sim, columns=header1, index=header1)
+
+    # Create a graph from the correlation matrix
+    G = nx.from_pandas_adjacency(df)
+
+    # Convert the networkx graph to an igraph graph
+    # Note the "mode" argument is set to 'UNDIRECTED'
+    ig_graph = ig.Graph.Adjacency((df.values > 0).tolist(),mode='UNDIRECTED')
+    ig_graph.es['weight'] = df.values[df.values > 0].flatten()
+
+    # print(ig_graph.es.attributes()) # check edge attributes- weight
+    # print(ig_graph.ecount()) # check edge count
+
+    # Define the resolution parameter: rs
+    # default rs= 1.0, Adjust this to your needs
+    # Map algorithm names to classes
+    algo_dict = {
+        'ModularityVertexPartition': leidenalg.ModularityVertexPartition,
+        'RBConfigurationVertexPartition': leidenalg.RBConfigurationVertexPartition,
+        'RBERVertexPartition': leidenalg.RBERVertexPartition,
+        'CPMVertexPartition': leidenalg.CPMVertexPartition,
+        'SurpriseVertexPartition': leidenalg.SurpriseVertexPartition,
+        'SignificanceVertexPartition': leidenalg.SignificanceVertexPartition
+        }
+
+    # Run the Leiden algorithm 
+    if algo=='SurpriseVertexPartition':
+        partition = leidenalg.find_partition(ig_graph,
+                                             algo_dict[algo],
+                                             weights='weight')
+    else:
+        partition = leidenalg.find_partition(ig_graph,
+                                             algo_dict[algo], 
+                                             weights='weight',
+                                             resolution_parameter=rs)
+
+    # Get a list of node names
+    node_names = list(df.index)
+
+    if edgecolormethod == 'gradient':
+        # gradient scale of grey for egdes based on edge weights (pearson correlation)
+        # darker edges are higher weights
+        # pearson correlation of 1 is colored black
+
+        # Normalize the weights to the range [0, 1]
+        weights_normalized = (ig_graph.es['weight'] - min(ig_graph.es['weight'])) / (max(ig_graph.es['weight']) - min(ig_graph.es['weight']))
+
+        # Map normalized weights to [0.1, 1] to avoid totally transparent edges
+        weights_mapped = 0.1 + 0.9 * weights_normalized
+
+        # Convert to shades of grey, where higher weights are darker
+        edge_colors = [(1 - w, 1 - w, 1 - w) for w in weights_mapped]
+
+        # Create edge widths based on the weights
+        edge_widths = [1 + 3 * w for w in weights_mapped]
+
+        # Use the Fruchterman-Reingold force-directed algorithm, considering edge weight
+        layout = ig_graph.layout('fr', weights='weight')
+
+        # Plot the graph with the edge colors and edge widths based on weight
+        ig.plot(partition, vertex_label=node_names,
+                layout=layout, edge_curved=False, edge_color=edge_colors,edge_width=edge_widths,
+                target=f'{outputpath}{outputname}_gradient_leiden_network.pdf')
+        
+    elif edgecolormethod == 'threshold':
+        # threshold for edge color based on edge weights (pearson correlation)
+        # edge weight below threshold is colored grey
+        # edge weight above threshold is colored black
+
+        # Define the threshold
+        threshold = edgethreshold
+
+        # Assign colors based on whether the weight is above or below the threshold
+        edge_colors = ["black" if w > threshold else "grey" for w in ig_graph.es['weight']]
+
+        # Create edge widths based on the weights
+        edge_widths = [4 if w > threshold else 1 for w in ig_graph.es['weight']]
+
+        # Use the Fruchterman-Reingold force-directed algorithm, considering edge weight
+        layout = ig_graph.layout('fr', weights='weight')
+
+        # Plot the graph with the edge colors based on the threshold
+        ig.plot(partition, vertex_label=node_names,
+                layout=layout, edge_curved=False, edge_color=edge_colors,edge_width=edge_widths,
+                target=f'{outputpath}{outputname}_threshold_leiden_network.pdf')
+        
+    else:
+        # if input does not match 'gradient' or 'threshold', use default 'gradient'
+        print("edgecolormethod must be either 'gradient' or 'threshold', use default 'gradient' now")
+        
+        # gradient scale of grey for egdes based on edge weights (pearson correlation)
+        # darker edges are higher weights
+        # pearson correlation of 1 is colored black
+
+        # Normalize the weights to the range [0, 1]
+        weights_normalized = (ig_graph.es['weight'] - min(ig_graph.es['weight'])) / (max(ig_graph.es['weight']) - min(ig_graph.es['weight']))
+
+        # Map normalized weights to [0.1, 1] to avoid totally transparent edges
+        weights_mapped = 0.1 + 0.9 * weights_normalized
+
+        # Convert to shades of grey, where higher weights are darker
+        edge_colors = [(1 - w, 1 - w, 1 - w) for w in weights_mapped]
+
+        # Create edge widths based on the weights
+        edge_widths = [1 + 3 * w for w in weights_mapped]
+
+        # Use the Fruchterman-Reingold force-directed algorithm, considering edge weight
+        layout = ig_graph.layout('fr', weights='weight')
+
+        # Plot the graph with the edge colors and edge widths based on weight
+        ig.plot(partition, vertex_label=node_names,
+                layout=layout, edge_curved=False, edge_color=edge_colors,edge_width=edge_widths,
+                target=f'{outputpath}{outputname}_gradient_leiden_network.pdf')
+        
+
+    if savecsv == True: 
+        # Initialize lists to hold labels and colors
+        labels = []
+        colors = []
+
+        # Iterate over each community
+        for i, community in enumerate(partition):
+            # For each node in the community, append the node label and community number
+            for node_index in community:
+                labels.append(node_names[node_index])
+                colors.append(i + 1)
+
+        # Create a DataFrame
+        df_out = pd.DataFrame({'Id': labels, 'Label': labels, 'Color': colors})
+
+        # Write nodes to CSV
+        df_out.to_csv(f'{outputpath}{outputname}_nodes_leiden.csv', index=False)
+
+        # Get edges
+        # Extract the upper triangle (excluding the diagonal)
+        mask = np.triu(np.ones(df.shape), k=1).astype(bool)
+
+        # Filter dataframe values using the mask and then melt
+        df_triu = df.where(mask).stack().reset_index()
+        df_triu.columns = ['Source', 'Target', 'Weight']
+
+        # Write edges to CSV
+        df_triu.to_csv(f'{outputpath}{outputname}_edges_leiden.csv', index=False)
+
+
+
+
---
+++ seekr/kmer_textplot.py
@@ -0,0 +1,163 @@
+###################################################################################################
+### Description:
+# highlight the input kmer words in the input sequences
+
+### Details:
+# plot 2 input sequences and several interested words (max 10)
+# limit the number of interested words to 10 as it is hard to distinguish more than 10 colors
+# align the sequence at the beginning and label sequence positions on the bottom
+# highlight in colors and bold the words of interest
+# for multiple words, if the words overlap, the overlapped characters will be highlighted in the color of the first word in the list
+# therefore arrange the words in the list based on the priority of the words, with the most important word at the beginning of the list
+
+### Input:
+# seq1file, seq2file: input sequence 1 and 2, both in fasta format, if seq1 and seq2 includes more than one sequence, only the first sequence will be plotted
+# please have unique header for seq1 and seq2, the header will be used as the name of the sequence in the plot
+# words: word of interest, max 10 words in the format of a list, example: ['ATTA','AAAA','ACTC','CCTT','GGCC'], if more than 10 words, only the first 10 will be plotted
+# color_vec: list of hex color for the words of interest (for example: ['#d62728','#e377c2','#ff7f0e']), default will use the 'tab10' pallette with rearrangements into a quasi-rainbow order
+# the order of default color: red, pink, orange, olive, green, cyan, blue, purple, brown, grey
+# if you want to use your own color vector, please make sure the length of the color vector is the same as the length of the words list
+# wraplen: wrapping length, how many words to wrap in each line, default is 60
+# char_spacing: space between characters in the plot, default is 0.1
+# line_spacing: line space between seq1, seq2 and number, default is 0.2
+# seqfontsize: sequence character font size, default is 42
+# numfontsize: sequence position number font size, default is 40
+# outpoutname: the name of the output file, default is 'test'
+# outputpath: the path of the output file, default is current working directory, example: '/Users/username/Desktop/'
+# plotformat: output format, default is 'pdf', other common options are 'png', 'jpg', 'svg', 'eps', 'tif', 'tiff', 'ps', 'webp'
+# you can check all the options here: https://matplotlib.org/stable/api/_as_gen/matplotlib.pyplot.savefig.html
+# or by running this code: matplotlib.pyplot.gcf().canvas.get_supported_filetypes()
+# plotdpi: resolution of the output file, default is 300
+
+### Output:
+# user defined format of file (pdf as default) of the highlighted text plot named as outputname_kmer_textplot.pdf
+
+### Example:
+# kmer_textplot(seq1file='test.fa', seq2file='test2.fa', words=['ATCG','GTAG','AAAA','GCGC'], 
+#               color_vec='default', wraplen=60, char_spacing=0.1, line_spacing=0.2,
+#               seqfontsize=52, numfontsize=40, outputname='test', outputpath='', plotformat='pdf', plotdpi=300)
+
+
+#######################################################################################################################
+import matplotlib.font_manager as font_manager
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+
+from seekr.fasta_reader import Reader as seekrReader
+
+# loop thru seq and find unique coordinates for the word that matches the input word
+def find_word_coordinates(seq, inputword):
+    word_length = len(inputword)
+    coords = []
+    
+    # Loop through each character of seq
+    for i in range(len(seq) - word_length + 1):
+        seqword = seq[i:i + word_length]
+        
+        # If there's a match, record the coordinates
+        if seqword == inputword:
+            coords.extend(list(range(i, i + word_length)))
+
+    # Convert to numpy array and get unique coordinates
+    unique_coords = np.unique(np.array(coords))
+
+    return unique_coords
+
+# generate color for input coord 
+def ass_color(coord, matched_seq, color_vec):
+
+    for n in range(len(matched_seq)):
+        if coord in matched_seq[n]:
+            return color_vec[n]
+
+
+
+def kmer_textplot(seq1file, seq2file, words, color_vec='default', wraplen=60, char_spacing=0.1, line_spacing=0.2, 
+                  seqfontsize=42, numfontsize=40, outputname='test', outputpath='', plotformat='pdf', plotdpi=300):
+
+    # read in seq1 and seq2
+    seq1 = seekrReader(seq1file).get_seqs()[0]
+    seq2 = seekrReader(seq2file).get_seqs()[0]
+        
+
+    header1=seekrReader(seq1file).get_headers()[0]
+    header2=seekrReader(seq2file).get_headers()[0]
+    # remove the '>' in the header
+    header1=header1[1:]
+    header2=header2[1:]
+
+    # find matched coordinates for each word
+    if len(words)<=10:
+        matched_seq1 = [find_word_coordinates(seq1, word) for word in words]
+        matched_seq2 = [find_word_coordinates(seq2, word) for word in words]
+    else: 
+        print("length of words list exceeds 10, plotting the first 10 only")
+        words = words[0:10]
+        matched_seq1 = [find_word_coordinates(seq1, word) for word in words]
+        matched_seq2 = [find_word_coordinates(seq2, word) for word in words]
+
+    # convert the list of lists to a 1D np array and only get the unique values
+    matched_seq1_fl = np.unique(np.array([item for sublist in matched_seq1 for item in sublist]))
+    matched_seq2_fl = np.unique(np.array([item for sublist in matched_seq2 for item in sublist]))
+
+    if color_vec == 'default':
+        color_vec = ['#d62728','#e377c2','#ff7f0e','#bcbd22','#2ca02c','#17becf','#1f77b4','#9467bd','#8c564b','#7f7f7f']
+        print('default color order: red, pink, orange, olive, green, cyan, blue, purple, brown, grey')
+    else: 
+        if len(color_vec) != len(words):
+            print('the length of color vector is not the same as the length of the words list, use default color now')
+            print('default color order: red, pink, orange, olive, green, cyan, blue, purple, brown, grey')
+            color_vec = ['#d62728','#e377c2','#ff7f0e','#bcbd22','#2ca02c','#17becf','#1f77b4','#9467bd','#8c564b','#7f7f7f']
+
+    wrapped_seq1 = [seq1[i:i+wraplen] for i in range(0, len(seq1), wraplen)]
+    wrapped_seq2 = [seq2[i:i+wraplen] for i in range(0, len(seq2), wraplen)]
+
+    # Determine the total number of lines to plot
+    total_lines = max(len(wrapped_seq1), len(wrapped_seq2))
+
+    # Create a figure and axis
+    fig, ax = plt.subplots(figsize=(wraplen/4, total_lines*1))  # 3 rows for each line
+
+    # use Airal for fonts
+    # Specify the path to your .ttf font file
+    font_path = 'arial.ttf'
+    # Register the font with matplotlib
+    font_manager.fontManager.addfont(font_path)
+    # Set the font properties
+    prop = font_manager.FontProperties(fname=font_path)
+    plt.rcParams['font.family'] = prop.get_name()
+    #plt.rcParams["axes.labelsize"] = 30
+
+    # make sure the plot font in pdf can be editted in using illustrator
+    mpl.rcParams['pdf.fonttype'] = 42
+
+    # Hide the x and y axis
+    ax.axis('off')
+
+    # Plot sequences and numbers
+    # for matched characters use red color and bold font
+    for i in range(total_lines):
+        for j in range(60):
+            
+            if i < len(wrapped_seq1) and j < len(wrapped_seq1[i]):
+                color = ass_color(i*wraplen+j, matched_seq1, color_vec) if i*wraplen+j in matched_seq1_fl else '#000000'
+                weight = 'bold' if i*wraplen+j in matched_seq1_fl else 'normal'
+                ax.text(j*char_spacing, total_lines*1 - 1*i - 0.1, wrapped_seq1[i][j], fontsize=seqfontsize, ha='center', va='center', color=color, weight=weight)
+                
+            if i < len(wrapped_seq2) and j < len(wrapped_seq2[i]):
+                color = ass_color(i*wraplen+j, matched_seq2, color_vec) if i*wraplen+j in matched_seq2_fl else '#000000'
+                weight = 'bold' if i*wraplen+j in matched_seq2_fl else 'normal'
+                ax.text(j*char_spacing, total_lines*1 - 1*i - 0.1-line_spacing, wrapped_seq2[i][j], fontsize=seqfontsize, ha='center', va='center', color=color, weight=weight)
+                
+            if i*wraplen + j < max(len(seq1), len(seq2)):
+                ax.text(j*char_spacing, total_lines*1 - 1*i - 0.1-line_spacing*2, str(i*wraplen+j+1), fontsize=numfontsize, ha='center', va='center')
+
+    #plt.show()
+    formatlist = list(plt.gcf().canvas.get_supported_filetypes())
+    if plotformat in formatlist: 
+        plt.savefig(f'{outputpath}{outputname}_kmer_textplot.{plotformat}', format=plotformat, dpi=plotdpi, bbox_inches='tight')
+    else: 
+        print("plotformat not supported. use default 'pdf' now. other common formats are: 'png', 'jpg', 'svg', 'eps', 'tif', 'tiff', 'ps', 'webp'")
+        plt.savefig(f'{outputpath}{outputname}_kmer_textplot.pdf', format='pdf', dpi=plotdpi, bbox_inches='tight')
+
