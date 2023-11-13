58f2d111951685421a4a27b67b81d3f82fb7edc9
Shuang <57366173+Shuang-Plum@users.noreply.github.com>
2023-11-13 12:36:57 -0500

    Add files via upload

---
+++ seekr/adj_pval.py
@@ -0,0 +1,134 @@
+###################################################################################################
+### Description: 
+# adjust p-values for multiple comparisons
+
+### Details:
+# this function performs multiple comparison correction for p values calculated by find_pval
+# multiple comparison correction is performed by statsmodels.stats.multitest.multipletests function
+# the input is a dataframe of p values, with the row names (input 1) and column names (input 2) as the headers of the input sequences
+# the output is a dataframe of adjusted p values, with the same dimension as the input dataframe
+# if the input dataframe is a symmetric matrix: the row and column (or input1 and input2 in find_pval) are the same
+# the number of multiple comparisons is automatically corrected to be half the matrix size, excluding the diagonal
+# that is only the upper triangle of the matrix (excluding diagonal) is used for multiple comparison correction
+# in the output dataframe, the upper triangle of the matrix (excluding diagonal) is filled with the adjusted p values
+# the lower triangle and the diagonal is filled with NaN
+# if the input dataframe is not a symmetric matrix the total matrix is used for multiple comparison correction
+
+### Input:
+# pvals: a dataframe of p values, with the row names (input 1) and column names (input 2) as the headers of the input sequences from find_pval
+# method: the method used for multiple comparison correction; can be 'bonferroni', 'sidak', 'holm-sidak', 'holm', 'simes-hochberg', 'hommel', 'fdr_bh', 'fdr_by', 'fdr_tsbh', 'fdr_tsbky'
+# please refer to statsmodels.stats.multitest.multipletests for details
+# alpha: the desired alpha level (family-wise error rate, FWER); default is 0.05
+# The FWER is the probability of making one or more false discoveries, or Type I errors, when performing multiple hypotheses tests
+# alpha represents the desired significance level for the entire set of tests, not just for an individual hypothesis test. 
+# For example, if you set alpha to 0.05, you are aiming to have no more than a 5% chance of making at least one Type I error across all the tests you are conducting
+# outputname: full path of the output file, default is None, which means the output is not saved to a csv file
+# outputname can be set to 'test_adj_pval' under current directory or another folder: '/Users/username/Desktop/test_adj_pval'
+# the trailing .csv will be automatically added to ensure the output is saved as a csv file
+
+
+### Output:
+# a dataframe of adjusted p values, with the row names (input 1) and column names (input 2) same as the input dataframe
+# if outputname is set to a string, the dataframe is saved to a csv file named as outputname.csv
+# if the input pvals is a symmetric matrix, meaning the row and column (or input1 and input2 in find_pval) are the same
+# the output dataframe will only contain adjusted pvals for the upper triangle of the matrix (excluding diagonal)
+# the rest of the matrix is set to NaN, as they are not used for multiple comparison correction
+
+### Example:
+# fitres = find_dist(inputseq='default', k_mer=4, log2='Log2.post', models='common10', subsetting=True, subset_size = 10000, fit_model=True, statsmethod='ks',progress_bar=True, plotfit=None, outputname='test_fitres')
+# pvals=find_pval(seq1file='test1.fa', seq2file='test2.fa', mean_path='bkg_mean_4mers.npy', std_path='bkg_std_4mers.npy', k_mer=4, fitres=fitres, log2='Log2.post', bestfit=1, outputname='test_pval', progress_bar=True)
+# adjpvals=adj_pval(pvals, method='bonferroni', alpha=0.05, outputname='test_adj_pval')
+
+########################################################################################################
+
+import pandas as pd
+import numpy as np
+import statsmodels.stats.multitest as ssm
+
+# pvals=pd.read_csv('test_pval_sym.csv',header=0, index_col=0)
+
+# Function to check if the DataFrame is symmetric
+def is_symmetric(df):
+    # Exclude the diagonal elements by filling them with NaN
+    df_no_diagonal = df.where(~np.eye(df.shape[0], dtype=bool))
+    # Check if DataFrame is symmetric
+    return df_no_diagonal.equals(df_no_diagonal.T)
+
+def adj_pval(pvals, method, alpha=0.05, outputname=None):
+
+    # check if pvals is a dataframe
+    if not isinstance(pvals, pd.DataFrame):
+        print('The input pvals is not a dataframe. Please check the input.')
+        return None
+    else:
+        # check if column and row numbers are the same for pvals
+        # as if they are not the same it is definitely not a symmetric matrix
+        if pvals.shape[0] == pvals.shape[1]:
+            # check if pvals is a symmetric matrix
+            if is_symmetric(pvals):
+                print('The input pvals is a symmetric matrix. Only the upper triangle of the matrix (excluding diagonal) is used for multiple comparison correction.')
+                # pvals is a symmetric matrix
+                # only get the upper triangle of the matrix, excluding the diagonal
+                # and flatten it to numpy array
+                upper_triangle_indices = np.triu_indices_from(pvals, k=1)
+                # Extract the p-values from the upper triangle
+                upper_tri_values = pvals.values[upper_triangle_indices]
+                # perform multiple comparison correction
+                adj_p_vals = ssm.multipletests(upper_tri_values, alpha=alpha, method=method)[1]
+    
+                # Create a new DataFrame to hold the adjusted p-values with the same shape as the original
+                adj_df = pd.DataFrame(np.nan, index=pvals.index, columns=pvals.columns)
+                
+                # Fill the upper triangle of the adjusted DataFrame with the adjusted p-values
+                adj_df.values[upper_triangle_indices] = adj_p_vals
+
+                if outputname:
+                    adj_df.to_csv(f'{outputname}.csv')
+                return adj_df
+            
+            else:
+                print('The input pvals is not a symmetric matrix. The total matrix is used for multiple comparison correction.')
+                # convert and faltten the dataframe
+                pvals_np = pvals.to_numpy()
+                p_flatten = pvals_np.flatten()
+
+                # Apply the adjustment method of your choice, for example, Bonferroni
+                adj_p_vals = ssm.multipletests(p_flatten, alpha=alpha, method=method)[1]
+
+                # Reshape the adjusted p-values back into the original matrix form
+                adj_df = adj_p_vals.reshape(pvals.shape)
+
+                # convert the np array to dataframe and add back the index and column names
+                adj_df = pd.DataFrame(adj_df, index=pvals.index, columns=pvals.columns)
+
+                if outputname:
+                    adj_df.to_csv(f'{outputname}.csv')
+                return adj_df
+            
+        else:
+            print('The input pvals is not a symmetric matrix. The total matrix is used for multiple comparison correction.')
+            # convert and faltten the dataframe
+            pvals_np = pvals.to_numpy()
+            p_flatten = pvals_np.flatten()
+
+            # Apply the adjustment method of your choice, for example, Bonferroni
+            adj_p_vals = ssm.multipletests(p_flatten, alpha=alpha, method=method)[1]
+
+            # Reshape the adjusted p-values back into the original matrix form
+            adj_df = adj_p_vals.reshape(pvals.shape)
+
+            # convert the np array to dataframe and add back the index and column names
+            adj_df = pd.DataFrame(adj_df, index=pvals.index, columns=pvals.columns)
+
+            if outputname:
+                adj_df.to_csv(f'{outputname}.csv')
+            return adj_df
+
+
+                
+
+        
+
+        
+
+
---
+++ seekr/kmer_count_barplot.py
@@ -0,0 +1,151 @@
+###################################################################################################
+### Description:
+# barplot of the transformed or raw z-score for kmer words of the input sequences
+# limit input to 10 sequences -- hard to choose more than 10 distinct colors
+
+### Details:
+# plot the z-score that is the output of BasicCounter, where user can define whether and how to do log2 transform
+# order kmer words by the summed difference from the mean among all sequences, can be descending or ascending
+# user define to plot the top x kmer words (could be messy if there are too many words)
+# x axis label is the kmer words
+
+### Input:
+# inputfile: fasta file with unique headers for each sequences, limit to 10 sequences at max, for more than 10 sequences input, only plot the first 10 sequences
+# mean: the route to your normalization mean vector file
+# std: the route to your normalization std vector file
+# k: the kmer size used in seekr, which should be consistent with the mean and std file
+# log2: whether to do log2 transform, possible options are: 'Log2.post','Log2.pre' and 'Log2.none', default is 'Log2.post'
+# Log2.post -- Log2 transformation post-standardization: self.counts += np.abs(np.min(self.counts)) self.counts += 1 self.counts = np.log2(self.counts)
+# Log2.pre -- Log2 transformation pre-standardization: self.counts += np.abs(np.min(self.counts)) self.counts += 1
+# Log2.none -- no log2 transformation
+# sortmethod: how to sort the kmer words based on summed difference from the mean among all sequences, possible options: 'ascending' (default) and 'descending'
+# topkmernumber: the number of sorted kmer words to plot, default is 10, if input number is more than total number of kmer words, plot all words
+# if you want to plot all kmer words, set topkmernumber to a extreme number such as 1000000
+# xlabelsize: x axis label font size, default is 20
+# ylabelsize: y axis label font size, default is 20
+# xticksize: x tick label font size, default is 20
+# yticksize: y tick label font size, default is 20
+# legendsize: legend font size, default is 12
+# outputname: the path and name to save the output file, default is current working directory 'test_kmer_count_barplot', other example: '/Users/username/Desktop/test_barplot'
+# pformat: the format of the output file, default is 'pdf'. Other options are: 'eps', 'jpeg', 'jpg', 'pgf', 'png', 'ps', 'raw', 'rgba', 'svg', 'svgz', 'tif', 'tiff', 'webp'
+# pdpi: the dpi of the output file, default is 300
+
+### Output:
+# barplot named as outputname.pformat
+
+### Example:
+# kmer_count_barplot(inputfile='test.fa', mean='gencodevM25_unique_mean_4mers.npy', 
+#                    std = 'gencodevM25_unique_std_4mers.npy', log2 = 'Log2.post', 
+#                    k=4, sortmethod='ascending', topkmernumber=10,
+#                    xlabelsize=20, ylabelsize=20, xticksize=20, yticksize=20, legendsize=12,
+#                    outputname='/Users/username/Downloads/test_kmer_count_barplot', pformat='pdf', pdpi=300)
+
+#######################################################################################################################
+import matplotlib.font_manager as font_manager
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import os
+
+import pandas as pd
+import seaborn as sns
+
+from seekr.kmer_counts import BasicCounter as seekrBasicCounter
+from seekr.fasta_reader import Reader as seekrReader
+
+
+def kmer_count_barplot(inputfile, mean, std, k, log2='Log2.post', sortmethod='ascending', topkmernumber=10,
+                       xlabelsize=20, ylabelsize=20, xticksize=20, yticksize=20, legendsize=12, 
+                       outputname='test_kmer_count_barplot',pformat='pdf',pdpi=300):
+
+    t1 = seekrBasicCounter(inputfile, mean=mean, std=std, log2=log2, k=k,silent=True)
+
+    t1.make_count_file()
+    header1=seekrReader(inputfile).get_headers()
+    # remove the '>' in the header
+    header1 = [i[1:] for i in header1]
+
+    # get counts
+    tt1=t1.counts
+
+    # check if there are more than 10 input sequences
+    if len(header1) > 10:
+        print("There are more than 10 input sequences, only plot the first 10 sequences")
+        header1 = header1[:10]
+        tt1 = tt1[:10]
+
+    # get kmers and convert to dataframe
+    kmer1=t1.kmers
+    df = pd.DataFrame(tt1, index=header1, columns=kmer1)
+
+
+    # Calculate the mean of all rows for each column
+    column_means = df.mean()
+
+    # Calculate summed absolute difference from mean for each column
+    if sortmethod == 'ascending':
+        summed_difference_from_mean = (df - column_means).abs().sum().sort_values(ascending=True)
+    elif sortmethod == 'descending':
+        summed_difference_from_mean = (df - column_means).abs().sum().sort_values(ascending=False)
+    else:
+        print("Please choose a sorting method: 'ascending' or 'descending', use default 'ascending' now")
+        summed_difference_from_mean = (df - column_means).abs().sum().sort_values(ascending=True)
+
+    # Reorder the dataframe based on the summed absolute difference from the mean
+    df_reordered = df[summed_difference_from_mean.index]
+
+
+    # Melt the dataframe to a long format
+    df_melted = df_reordered.reset_index().melt(id_vars='index', value_vars=df_reordered.columns)
+    df_melted.columns = ['Sample', 'Kword', 'Value']
+
+    # calculate the number of input sequences
+    ttlnum = topkmernumber*len(header1)
+
+    if ttlnum > len(df_melted):
+        print(f"Only {int(len(df_melted)/len(header1))} kmer words, less than {topkmernumber} words you want to plot, plot all words")
+        df_melted_plot=df_melted
+    else:
+        df_melted_plot=df_melted[:ttlnum]
+
+    # Plot
+    plt.figure(figsize=((topkmernumber*2), 8))
+
+    # use Airal for fonts
+    # Specify the path to your .ttf font file
+    # Get the directory where the current file is located.
+    current_dir = os.path.dirname(os.path.realpath(__file__))
+
+    # Construct the path to the .ttf file.
+    font_path = os.path.join(current_dir, 'data', 'arial.ttf')
+    # Register the font with matplotlib
+    font_manager.fontManager.addfont(font_path)
+    # Set the font properties
+    prop = font_manager.FontProperties(fname=font_path)
+    plt.rcParams['font.family'] = prop.get_name()
+    # make sure the plot font in pdf can be editted in using illustrator
+    mpl.rcParams['pdf.fonttype'] = 42
+
+    # plot the barplot
+    sns.barplot(x='Kword', y='Value', hue='Sample', data=df_melted_plot, palette="tab10")
+
+    # Setting x and y axis labels
+    plt.xlabel('Kmer Words', fontsize=xlabelsize)
+    plt.ylabel('z-score (transformed or raw)', fontsize=ylabelsize)
+
+    # Adjusting tick font sizes
+    plt.xticks(rotation=90, fontsize=xticksize)
+    plt.yticks(fontsize=yticksize)
+
+    # Place the legend to the right of the plot
+    plt.legend(loc='center left', bbox_to_anchor=(1, 0.5), fontsize=legendsize)
+
+
+    #plt.show()
+    formatlist = list(plt.gcf().canvas.get_supported_filetypes())
+    if pformat in formatlist:
+        plt.savefig(f'{outputname}.{pformat}', format=pformat, dpi=pdpi, bbox_inches='tight')
+    else:
+        print("plotformat not supported. use default 'pdf' now. other common formats are: 'png', 'jpg', 'svg', 'eps', 'tif', 'tiff', 'ps', 'webp'")
+        plt.savefig(f'{outputname}.pdf', format='pdf', dpi=pdpi, bbox_inches='tight')
+
+
