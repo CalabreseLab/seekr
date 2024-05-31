d7572588651c615c8668fdbc8fc7c145fe3905c4
Shuang <57366173+Shuang-Plum@users.noreply.github.com>
2024-05-31 13:30:26 -0400

    Update __version__.py

--- seekr/__version__.py
+++ seekr/__version__.py
@@ -1,7 +1,7 @@
 __title__ = "seekr"
 __description__ = "Count small kmer frequencies in nucleotide sequences."
 __url__ = "https://github.com/CalabreseLab/seekr"
-__version__ = "2.0.0"
+__version__ = "2.0.1"
 __author__ = "Jessime Kirk, Shuang Li, Luke Ni"
 __author_email__ = "shuang9@email.unc.edu"
 __license__ = "MIT"
